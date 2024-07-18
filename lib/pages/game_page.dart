1908b923d35ad8ba12fb2f8e7661d5ca8ba17d64
shcrowfield <mikipoti@gmail.com>
2024-07-18 17:02:27 +0200

    0.7.1

--- lib/pages/game_page.dart
+++ lib/pages/game_page.dart
@@ -1,6 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:thatsnot/alert_dialogs.dart';
+//import 'package:thatsnot/alert_dialogs.dart';
 import 'package:thatsnot/pages/start_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:thatsnot/services/database.dart';
